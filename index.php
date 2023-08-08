96fd19705794e02b8bd163067dc1f2e7af5a1e2c
tharun-reddy-8118 <141704173+tharun-reddy-8118@users.noreply.github.com>
2023-08-08 11:19:14 +0530

    added index

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="main-div">
+        <div class="navbar">
+            <img src="logo3.png" class="logo">
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">ABOUT US</a>
+                </li>
+                <li>
+                    <a href="#">FAQ'S</a>
+                </li>
+                <li>
+                    <a href="3">CONTACT</a>
+                </li>
+                <li>
+                    
+                </li>
+            </ul>
+        </div>
+        <div class="content">
+            <h1>WELCOME TO SKYINFO</h1>
+            <div>
+                <a href="./login1.php"><button type="submit"><span></span>LOGIN</button></a>
+                <a href="./signup1.php"><button type="submit"><span></span>SIGN UP/REGISTER</button></a>
+            </div>
+        </div>
+       
+
+    </div>
+</body>
+</html>
